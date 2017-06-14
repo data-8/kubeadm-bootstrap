91091f5eafcb8d1b0f750989d28dbc6fcaea3d80
yuvipanda <yuvipanda@gmail.com>
2017-06-14 11:20:14 -0700

    Add link to kubeadm bug report

--- init-kubeadm-master.bash
+++ init-kubeadm-master.bash
@@ -4,7 +4,9 @@ set -e
 source data/config.bash
 source data/secrets.bash
 
-kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address="${KUBE_MASTER_IP}" --token="${KUBEADM_TOKEN}"
-
 echo "After this is done, you need to manually edit /etc/kubernetes/manifests/kube-apiserver.yaml"
 echo "Adding --bind-address=${KUBE_MASTER_IP} to the command of the container, right below --advertise-address"
+echo "Should be fixed by https://github.com/kubernetes/kubeadm/issues/305"
+
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address="${KUBE_MASTER_IP}" --token="${KUBEADM_TOKEN}"
+
