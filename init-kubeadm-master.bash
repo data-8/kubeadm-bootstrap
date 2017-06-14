4656af76e8156f5500256556713447641b6b7040
yuvipanda <yuvipanda@gmail.com>
2017-06-14 12:15:13 -0700

    Add another note about init-ing master

--- init-kubeadm-master.bash
+++ init-kubeadm-master.bash
@@ -6,6 +6,7 @@ source data/secrets.bash
 
 echo "After this is done, you need to manually edit /etc/kubernetes/manifests/kube-apiserver.yaml"
 echo "Adding --bind-address=${KUBE_MASTER_IP} to the command of the container, right below --advertise-address"
+echo "Alsos change the host of the health chck to ${KUBE_MASTER_IP} from 127.0.0.1"
 echo "Should be fixed by https://github.com/kubernetes/kubeadm/issues/305"
 
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address="${KUBE_MASTER_IP}" --token="${KUBEADM_TOKEN}"
