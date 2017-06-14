d8603e3838687f15dcb495b43876fd46603b8dcb
yuvipanda <yuvipanda@gmail.com>
2017-06-14 13:18:38 -0700

    Add script to install support charts

---
+++ install-support.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Install support chart, and get nginx-ingress to work
+
+helm install --name=support --namespace=support support/
+
+# Until https://github.com/kubernetes/charts/pull/1250 gets merged
+kubectl --namespace=support patch deployment support-nginx-ingress-controller -p '{"spec": {"template": { "spec": { "hostNetwork": true } } } } 
