94f79825c49510ac0bab29eb5ec883f7cc2d98ee
yuvipanda <yuvipanda@gmail.com>
2017-06-14 11:09:05 -0700

    Update init-kubeadm-master

---
+++ data/secrets.sample.bash
@@ -0,0 +1,1 @@
+KUBEADM_TOKEN="output of `pwgen -A 6 1`.`pwgen -A 16 1` "
--- init-kubeadm-master.bash
+++ init-kubeadm-master.bash
@@ -1,2 +1,10 @@
 #!/bin/bash
+set -e
+
+source data/config.bash
+source data/secrets.bash
+
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address="${KUBE_MASTER_IP}" --token="${KUBEADM_TOKEN}"
+
+echo "After this is done, you need to manually edit /etc/kubernetes/manifests/kube-apiserver.yaml"
+echo "Adding --bind-address=${KUBE_MASTER_IP} to the command of the container, right below --advertise-address"
