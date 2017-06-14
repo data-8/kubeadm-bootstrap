61efcb794e935829d80302bd0abd6a5299b1573c
yuvipanda <yuvipanda@gmail.com>
2017-06-14 13:10:19 -0700

    Add helm init command to install helm

--- install-helm.bash
+++ install-helm.bash
@@ -5,3 +5,6 @@ set -e
 curl https://storage.googleapis.com/kubernetes-helm/helm-v2.4.2-linux-amd64.tar.gz | tar xvz
 mv linux-amd64/helm /usr/local/bin
 rm -rf linux-amd64
+
+/usr/local/bin/helm init
+
