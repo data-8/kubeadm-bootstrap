14742f3350ee8533291857a3859a1d2508d65e14
yuvipanda <yuvipanda@gmail.com>
2017-06-14 14:29:05 -0700

    Roll run-script into join-master

--- join-master.bash
+++ join-master.bash
@@ -3,4 +3,14 @@
 set -e
 NODES="${1}"
 
+CMD=$(cat <<EOCMD
+cd /root;
+if [ ! -d /root/haas-infrastructure ]; then git clone https://github.com/data-8/haas-infrastructure.git; fi;
+cd haas-infrastructure
+git fetch --quiet && git reset --hard --quiet origin/master
+./install-kubeadm.bash
+EOCMD
+)
+
+sudo clush -w "${NODES}" "${CMD}"
 sudo clush -w "${NODES}" kubeadm join --token "${KUBEADM_TOKEN}"  "${KUBE_MASTER_IP}":6443
