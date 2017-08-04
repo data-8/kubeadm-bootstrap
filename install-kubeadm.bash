361768c58cebcf76458bae609d4dfd257c8261b8
yuvipanda <yuvipanda@gmail.com>
2017-08-03 22:50:57 -0700

    Upgrade & pin version of docker so we can use overlay2
    overlay only let us use about 20-30% of the underlying
    disk, because of terrible inode exhaustion issues

--- install-kubeadm.bash
+++ install-kubeadm.bash
@@ -1,22 +1,20 @@
 #!/bin/bash
-apt-get update 
+apt-get update
 apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
+deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable
 EOF
+
 apt-get update
 
-# Install docker if you don't have it already.
-
-apt-get install -y docker-engine
-
-# Make sure you're using the overlay driver!
-# Note that this gives us docker 1.11, which does *not* support overlay2
+apt-get install -y docker-ce=17.06.0~ce-0~ubuntu
 
 systemctl stop docker
 modprobe overlay
-echo '{"storage-driver": "overlay"}' > /etc/docker/daemon.json
+echo '{"storage-driver": "overlay2"}' > /etc/docker/daemon.json
 rm -rf /var/lib/docker/*
 systemctl start docker
 
